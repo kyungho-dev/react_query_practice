3fd2c2feeb8ce8131da4abe6ed3ed8ec842eb2a1
kyungho-dev <kyunghoma2@naver.com>
2023-02-27 20:03:36 +0900

    #Chap13 : Pagination생성 (pre-fetching 없는 버전 => ux 안좋은 버전)

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -5,15 +5,15 @@ import { useQuery } from 'react-query';
 import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
 
-async function fetchPosts() {
+async function fetchPosts(pageNum) {
   const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`
   );
   return response.json();
 }
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
@@ -30,7 +30,10 @@ export function Posts() {
 
   // 세번째 인자는 "옵션" 이다.
 
-  const { data, isError, error, isLoading } = useQuery("posts", fetchPosts, {
+  const { data, isError, error, isLoading } = useQuery(
+    ["posts", currentPage],
+    () => fetchPosts(currentPage),
+    {
     staleTime: 2000
   });
   // 세번째 인자는 옵션이고, staleTime은 밀리세컨드(ms) 기준이므로 2000이면 2초이다.
@@ -63,11 +66,21 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage <= 1}
+          onClick={() => {
+            setCurrentPage(previousValue => previousValue - 1);
+          }}
+        >
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button
+          disabled={currentPage >= maxPostPage}
+          onClick={() => {
+            setCurrentPage(previousValue => previousValue + 1);
+          }}
+        >
           Next page
         </button>
       </div>
