2f0f27e5b592109c0b41d0ce7e977cb793857e46
kyungho-dev <kyunghoma2@naver.com>
2023-03-01 17:23:26 +0900

    #Chap 17 : useMutation 사용하기

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,4 +1,4 @@
-import {useQuery} from "react-query";
+import { useQuery, useMutation } from "react-query";
 
 async function fetchComments(postId) {
   const response = await fetch(
@@ -27,6 +27,14 @@ export function PostDetail({ post }) {
   // replace with useQuery
   const { data, isLoading, isError, error } = useQuery(['comments', post.id], () => fetchComments(post.id));
 
+
+  // 삭제 로직
+  // useMutation은 mutate 함수(mutate()) 를 반환하게 된다.
+  // useQuery와 달리 (첫번째 인자로) 쿼리키를 받지 않는다.
+  // 함수에 인자를 넣을 수 있다. (useMutation((postId) => dele... 앞에 useMutation((이부분) =>
+  const deleteMutation = useMutation((postId) => deletePost(postId));
+
+
   if (isLoading) return <h3>Now Loading...</h3>;
   if (isError) return (
     <>
@@ -38,7 +46,17 @@ export function PostDetail({ post }) {
   return (
     <>
       <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+      <button onClick={() => deleteMutation.mutate(post.id)}>Delete</button>
+      {
+        deleteMutation.isError && <p style={{color: 'red' }}>Error deleting the post</p>
+      }
+      {
+        deleteMutation.isLoading && <p style={{color: 'purple' }}>Deleting the post</p>
+      }
+      {
+        deleteMutation.isSuccess && <p style={{color: 'green' }}>Post has (not) been delete :) </p>
+      }
+      <button>Update title</button>
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map((comment) => (
