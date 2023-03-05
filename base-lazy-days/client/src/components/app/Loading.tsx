c668b6d9ffa1f9909bb51635629d4240e1e38cac
kyungho-dev <kyunghoma2@naver.com>
2023-03-05 23:35:35 +0900

    #Chap 36 : useQuery의 onError 사용하기

--- base-lazy-days/client/src/components/app/Loading.tsx
+++ base-lazy-days/client/src/components/app/Loading.tsx
@@ -1,9 +1,15 @@
 import { Spinner, Text } from '@chakra-ui/react';
 import { ReactElement } from 'react';
+import { useIsFetching } from 'react-query';
 
 export function Loading(): ReactElement {
   // will use React Query `useIsFetching` to determine whether or not to display
-  const isFetching = false; // for now, just don't display
+  const isFetching = useIsFetching();
+  // useIsFetching은 현재 가져오기 상태인 쿼리 호출의 수를 나타내는 정수 값을 반환한다.
+  // isFetching이 0보다 크면가져오기 상태인 호출이 존재하며 falsy하지 않은(truthy) 값이 되겠지.
+  // 이 경우 아래 display가 inherit이 될꺼고, 이러면 로딩 스피너 표시 될것
+  // 반면, 가져오는 데이터가 없다면 0이 되고, 0은 falsy한 값이므로
+  // display가 none 이 되겠지
 
   const display = isFetching ? 'inherit' : 'none';
 
--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -12,8 +12,20 @@ async function getTreatments(): Promise<Treatment[]> {
 }
 
 export function useTreatments(): Treatment[] {
+  const toast = useCustomToast();
+
   const fallback = [];
   // 빈 배열을 만들어 준 후, 아래처럼 해주면 data의 default 값이 fallback이 되는 것
-  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments);
+  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments, {
+    onError: (error) => {
+      // error가 어느 타입이든 올 수 있기 때문에 error의 타입을 확인하는 과정이 필요
+      const title =
+        error instanceof Error // error가 자바스크립트 Error 클래스의 인스턴스 라면?
+          ? error.message
+          : 'error connecting to server';
+      // 그리고 toast를 호출한 후 몇가지 옵션
+      toast({ title, status: 'error' });
+    },
+  });
   return data;
 }
