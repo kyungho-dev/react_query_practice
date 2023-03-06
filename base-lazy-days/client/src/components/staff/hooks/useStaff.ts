4fd37ef7a2bc992079a335296131c48fb5423c46
kyungho-dev <kyunghoma2@naver.com>
2023-03-06 20:14:51 +0900

    #Chap 38 : useStaff에 중앙처리한 error 처리 적용

--- base-lazy-days/client/src/components/staff/hooks/useStaff.ts
+++ base-lazy-days/client/src/components/staff/hooks/useStaff.ts
@@ -4,12 +4,13 @@ import type { Staff } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
 import { filterByTreatment } from '../utils';
+import {useQuery} from "react-query";
 
 // for when we need a query function for useQuery
-// async function getStaff(): Promise<Staff[]> {
-//   const { data } = await axiosInstance.get('/staff');
-//   return data;
-// }
+async function getStaff(): Promise<Staff[]> {
+  const { data } = await axiosInstance.get('/staff');
+  return data;
+}
 
 interface UseStaff {
   staff: Staff[];
@@ -22,7 +23,12 @@ export function useStaff(): UseStaff {
   const [filter, setFilter] = useState('all');
 
   // TODO: get data from server via useQuery
-  const staff = [];
+  const fallback = [];
+
+  // 아래처럼 구조 분해 할당시
+  // { data: staff  이런식으로 해주면, data라는걸 구조분해로 받은뒤에
+  // staff 라는 이름에 담아준다.
+  const { data: staff = fallback } = useQuery(queryKeys.staff, getStaff);
 
   return { staff, filter, setFilter };
 }
