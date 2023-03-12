9b704912bff850c9d9a947c882f3ce3c3dbd4e6a
kyungho-dev <kyunghoma2@naver.com>
2023-03-12 21:14:41 +0900

    #Chap 55 : useQuery select 옵션 실습 적용

--- base-lazy-days/client/src/components/staff/hooks/useStaff.ts
+++ base-lazy-days/client/src/components/staff/hooks/useStaff.ts
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction, useState } from 'react';
+import { Dispatch, SetStateAction, useCallback, useState } from 'react';
 import { useQuery } from 'react-query';
 
 import type { Staff } from '../../../../../shared/types';
@@ -22,13 +22,20 @@ export function useStaff(): UseStaff {
   // for filtering staff by treatment
   const [filter, setFilter] = useState('all');
 
+  const selectFn = useCallback(
+    (unfilteredStaff) => filterByTreatment(unfilteredStaff, filter),
+    [filter],
+  );
+
   // TODO: get data from server via useQuery
   const fallback = [];
 
   // 아래처럼 구조 분해 할당시
   // { data: staff  이런식으로 해주면, data라는걸 구조분해로 받은뒤에
   // staff 라는 이름에 담아준다.
-  const { data: staff = fallback } = useQuery(queryKeys.staff, getStaff);
+  const { data: staff = fallback } = useQuery(queryKeys.staff, getStaff, {
+    select: filter === 'all' ? undefined : selectFn,
+  });
 
   return { staff, filter, setFilter };
 }
