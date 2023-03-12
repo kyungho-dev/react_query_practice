1a60253f4ef797c8b6b6f5074e12c51229b59a0c
kyungho-dev <kyunghoma2@naver.com>
2023-03-12 22:00:00 +0900

    #Chap 57 : refetch 옵션 적용하기

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -29,7 +29,18 @@ export function useTreatments(): Treatment[] {
   // });
 
   // queryClient에서 만든 에러 핸들러 사용하기 위해 onError 제거
-  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments);
+  // refetching 설정을 위한 세번째 인자 추가
+  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments, {
+    staleTime: 600000, // 10 minutes
+    // 참고로 캐싱타임의 기본값은 5분이다.
+    // 만료시간이 캐싱시간보다 긴 것은 말이 안된다.
+    // 만료된 데이터를 불러오는 동안 캐싱에 백업된 내용이 보여질 것.
+    // 그러므로 캐싱시간도 늘려줘야함.
+    cacheTime: 900000, // 15 minutes
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    refetchOnReconnect: false,
+  });
   return data;
 }
 
@@ -45,6 +56,10 @@ export function usePrefetchTreatments(): void {
   // prefetchQuery로 쿼리키를 사용(위에 사용한 useQuery의 키와 같은걸 사용)
   // 여기에서 키는 캐시에서 어느 useQuery가 이 데이터를 찾아야 하는지 알려주므로
   // 매우 중요하다
-  queryClient.prefetchQuery(queryKeys.treatments, getTreatments);
+  queryClient.prefetchQuery(queryKeys.treatments, getTreatments, {
+    staleTime: 600000,
+    cacheTime: 900000, // prefetch 옵션에 staleTime과 cacheTime 옵션을 추가해주면
+    // prefetching이 staleTIme과 cacheTime을 참조하여, 데이터의 새로 고침 여부를 결정하게 된다.
+  });
 }
 // 이렇게 만든 훅을 Home에서 사용할것
