4d448fe2ea6da362533a88d2a33375e63eda5668
kyungho-dev <kyunghoma2@naver.com>
2023-03-14 21:23:47 +0900

    #Chap 63 : useQuery와 auth 설정

--- base-lazy-days/client/src/components/user/hooks/useUser.ts
+++ base-lazy-days/client/src/components/user/hooks/useUser.ts
@@ -1,4 +1,5 @@
 import { AxiosResponse } from 'axios';
+import { useQuery } from 'react-query';
 
 import type { User } from '../../../../../shared/types';
 import { axiosInstance, getJWTHeader } from '../../../axiosInstance';
@@ -9,16 +10,16 @@ import {
   setStoredUser,
 } from '../../../user-storage';
 
-// async function getUser(user: User | null): Promise<User | null> {
-//   if (!user) return null;
-//   const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
-//     `/user/${user.id}`,
-//     {
-//       headers: getJWTHeader(user),
-//     },
-//   );
-//   return data.user;
-// }
+async function getUser(user: User | null): Promise<User | null> {
+  if (!user) return null;
+  const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
+    `/user/${user.id}`,
+    {
+      headers: getJWTHeader(user),
+    },
+  );
+  return data.user;
+}
 
 interface UseUser {
   user: User | null;
@@ -28,7 +29,13 @@ interface UseUser {
 
 export function useUser(): UseUser {
   // TODO: call useQuery to update user data from server
-  const user = null;
+  // const user = null;
+  // data: user 해줌으로써 data라는걸 구조분해할당받고 그걸 user라는 이름으로
+  // rename 해준것
+  const { data: user } = useQuery(
+    queryKeys.user, // 첫번째 인수는 쿼리키,
+    () => getUser(user), //  두번째 인수는 쿼리 함수
+  );
 
   // meant to be called from useAuth
   function updateUser(newUser: User): void {
