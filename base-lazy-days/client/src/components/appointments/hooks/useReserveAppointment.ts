874590f510fe25673e48a1d80aa90edf2c8abd49
kyungho-dev <kyunghoma2@naver.com>
2023-03-17 21:36:22 +0900

    #Chap 72 : useMutation을 이용한 mutate 함수 설정

--- base-lazy-days/client/src/components/appointments/hooks/useReserveAppointment.ts
+++ base-lazy-days/client/src/components/appointments/hooks/useReserveAppointment.ts
@@ -1,3 +1,5 @@
+import { useMutation } from 'react-query';
+
 import { Appointment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
@@ -5,17 +7,17 @@ import { useCustomToast } from '../../app/hooks/useCustomToast';
 import { useUser } from '../../user/hooks/useUser';
 
 // for when we need functions for useMutation
-// async function setAppointmentUser(
-//   appointment: Appointment,
-//   userId: number | undefined,
-// ): Promise<void> {
-//   if (!userId) return;
-//   const patchOp = appointment.userId ? 'replace' : 'add';
-//   const patchData = [{ op: patchOp, path: '/userId', value: userId }];
-//   await axiosInstance.patch(`/appointment/${appointment.id}`, {
-//     data: patchData,
-//   });
-// }
+async function setAppointmentUser(
+  appointment: Appointment,
+  userId: number | undefined,
+): Promise<void> {
+  if (!userId) return;
+  const patchOp = appointment.userId ? 'replace' : 'add';
+  const patchData = [{ op: patchOp, path: '/userId', value: userId }];
+  await axiosInstance.patch(`/appointment/${appointment.id}`, {
+    data: patchData,
+  });
+}
 
 // TODO: update type for React Query mutate function
 type AppointmentMutationFunction = (appointment: Appointment) => void;
@@ -24,8 +26,13 @@ export function useReserveAppointment(): AppointmentMutationFunction {
   const { user } = useUser();
   const toast = useCustomToast();
 
-  // TODO: replace with mutate function
-  return (appointment: Appointment) => {
-    // nothing to see here
-  };
+  // return에서 mutate 함수를 구조 분해 할당
+  //  useMutation 에는 쿼리키 필요 없다.
+  // 중요! 캐시에 있는 쿼리와는 관련 없음!
+  // 변이 함수만 있으면 된다!
+  const { mutate } = useMutation((appointment) =>
+    setAppointmentUser(appointment, user?.id),
+  );
+
+  return mutate;
 }
--- base-lazy-days/client/src/react-query/queryClient.ts
+++ base-lazy-days/client/src/react-query/queryClient.ts
@@ -34,7 +34,6 @@ export const queryClient = new QueryClient({
     },
     mutations: {
       onError: queryErrorHandler,
-
     },
   },
 });
