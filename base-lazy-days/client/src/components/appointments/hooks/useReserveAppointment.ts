2eca64543c8a6cdc3684cda65dbaa0de3424868c
kyungho-dev <kyunghoma2@naver.com>
2023-03-18 21:19:20 +0900

    #Chap 73 : mutate 함수를 위한 typescript 설정 (제네릭, 타입 설정)

--- base-lazy-days/client/src/components/appointments/hooks/useReserveAppointment.ts
+++ base-lazy-days/client/src/components/appointments/hooks/useReserveAppointment.ts
@@ -1,4 +1,4 @@
-import { useMutation } from 'react-query';
+import {UseMutateFunction, useMutation} from 'react-query';
 
 import { Appointment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
@@ -20,9 +20,21 @@ async function setAppointmentUser(
 }
 
 // TODO: update type for React Query mutate function
-type AppointmentMutationFunction = (appointment: Appointment) => void;
+// type AppointmentMutationFunction = (appointment: Appointment) => void;
 
-export function useReserveAppointment(): AppointmentMutationFunction {
+// useMutateFunction의 제네릭으로 설정하는 타입
+// 첫번째 : mutate 함수에서 반환된 데이터 -> void
+// 두번째 : Error 타입으로 -> unknown -> onError 핸들러 확인
+// 세번째 : mutate 함수에서 내부의 함수(아래에서는 setAppointmentUser)로
+//  보내줄 변수 -> 여기서는 Appointment 타입! (위에서 import 해온 타입임)
+// 마지막은 컨텍스트 타입 : 우선 unknow으로 해두고
+// 다음에 optimistic update(낙관적 업데이트) 할떄 채워 넣을 예정
+export function useReserveAppointment(): UseMutateFunction<
+  void,
+  unknown,
+  Appointment,
+  unknown
+> {
   const { user } = useUser();
   const toast = useCustomToast();
 
@@ -30,7 +42,7 @@ export function useReserveAppointment(): AppointmentMutationFunction {
   //  useMutation 에는 쿼리키 필요 없다.
   // 중요! 캐시에 있는 쿼리와는 관련 없음!
   // 변이 함수만 있으면 된다!
-  const { mutate } = useMutation((appointment) =>
+  const { mutate } = useMutation((appointment: Appointment) =>
     setAppointmentUser(appointment, user?.id),
   );
 
