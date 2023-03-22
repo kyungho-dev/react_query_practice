bd2589d87a73c6a77e143c8e12e145ff437a5614
kyungho-dev <kyunghoma2@naver.com>
2023-03-22 22:57:09 +0900

    #Chap85 : test코드를 위한 QueryClientProvider 생성

--- base-lazy-days/client/src/components/treatments/tests/Treatments.test.tsx
+++ base-lazy-days/client/src/components/treatments/tests/Treatments.test.tsx
@@ -1,7 +1,10 @@
 import { screen } from '@testing-library/react';
 
+import { renderWithQueryClient } from '../../../test-utils';
 import { Treatments } from '../Treatments';
 
-test('renders response from query', () => {
-  // write test here
+test('renders response from query', async () => {
+  // 클라이언트를 제공하는 provider에 래핑된
+  // treatment를 렌더링 할 것
+  renderWithQueryClient(<Treatments />);
 });
--- base-lazy-days/client/src/test-utils/index.tsx
+++ base-lazy-days/client/src/test-utils/index.tsx
@@ -1,11 +1,36 @@
-import { render } from '@testing-library/react';
+import { render, RenderResult } from '@testing-library/react';
+import { ReactElement } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 // import { defaultQueryClientOptions } from '../react-query/queryClient';
 
+// make a function to generate a unique query client for each test
+const generateQueryClient = () => {
+  return new QueryClient();
+};
+
+export function renderWithQueryClient(
+  ui: ReactElement,
+  client?: QueryClient,
+): RenderResult {
+  // 여기에서 렌더링 결과, 테스팅 라이브러리 렌더링 결과를 반환할 것
+  // 이 함수의 역할은 그 결과를 아직 얻지 못했을 경우 , 쿼리 클라이언트를 생성하는 것
+  // 따라서 queryClient를 알 수 없는 경우 client와 동일하게 설정하거나
+  // 아니면 client를 알 수 있는 경우 generateQueryClient를 실행해
+  // 새로 생성
+  const queryClient = client ?? generateQueryClient();
+
+  // 우선 provider를 render해줘야하는데
+  // 이 provider는 ui를 queryClient와 래핑한다.
+  return render(
+    <QueryClientProvider client={queryClient}>{ui}</QueryClientProvider>,
+  );
+}
+
 // from https://tkdodo.eu/blog/testing-react-query#for-custom-hooks
-// export const createQueryClientWrapper = (): React.FC => {
-//   const queryClient = generateQueryClient();
-//   return ({ children }) => (
-//     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-//   );
-// };
+export const createQueryClientWrapper = (): React.FC => {
+  const queryClient = generateQueryClient();
+  return ({ children }) => (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
