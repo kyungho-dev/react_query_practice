0261c1010224b8e1c0f8e20f7283e32c23bd56de
kyungho-dev <kyunghoma2@naver.com>
2023-03-23 19:51:53 +0900

    #Chap86 : 렌더링 된 쿼리 데이터 테스트하기 (screen과 expect 사용)

--- base-lazy-days/client/src/components/treatments/tests/Treatments.test.tsx
+++ base-lazy-days/client/src/components/treatments/tests/Treatments.test.tsx
@@ -7,4 +7,14 @@ test('renders response from query', async () => {
   // 클라이언트를 제공하는 provider에 래핑된
   // treatment를 렌더링 할 것
   renderWithQueryClient(<Treatments />);
+
+  // screen은 렌더링 결과에 접근하는 방법
+  // findBy 는 우리가 대기하고 있음을 뜻함
+  // 즉, 비동기식!
+  // 이 role의 이름을 "heading"이 될것
+  const treatmentTitles = await screen.findAllByRole('heading', {
+    name: /massage|facial|scrub/i,
+  });
+
+  expect(treatmentTitles).toHaveLength(3);
 });
